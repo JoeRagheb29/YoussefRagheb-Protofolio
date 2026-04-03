92a6e1260ddcc34a658d5490a1832f2f871a3873
joeragheb29 <youssifragheb838@gmail.com>
2026-04-03 21:42:38 +0200

    App component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './layout/Navbar'
+import Container from './layout/Container'
+import Footer from './layout/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Container />
+      <Footer />
+    </>
+  )
+}
+
+export default App
