2fa2652edce1e3a0fbf0c2e1654951fc8634dccc
joeragheb29 <youssifragheb838@gmail.com>
2026-04-17 15:52:43 +0200

    add last project

--- src/Sections/Projects.jsx
+++ src/Sections/Projects.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import productBuilder from '../assets/product-builder.png'
 import EventSystem from '../assets/event-attendance-system.png'
 import mohamedWael from '../assets/mohamed-wael.png'
+import portfolio from '../assets/myPortfolio.png'
 
 function Projects() {
   const projects = [
+    {
+      title: "Youssef Ragheb Portfolio",
+      description: "A personal portfolio website showcasing my projects, skills, and experience. Features a modern design with smooth scrolling, responsive layout, and interactive elements. Built with React and Tailwind CSS.",
+      tags: ["React", "Tailwind CSS" , "EmailJS" , "Vercel" , "Responsive Design"],
+      image: portfolio
+    },
     {
       title: "Event Attendance System",
       description: "A smart attendance tracking platform featuring secure role-based authentication, member attendance tracking (Present/Absent per session), individual attendance reports with engagement rates, full CRUD operations for members and sessions, real-time dashboard analytics, and light/dark mode support. Architected with a migration from SQLite to PostgreSQL via Supabase for scalability. Fully built independently.",
@@ -12,16 +19,16 @@ function Projects() {
       image: EventSystem
     },
     {
-      title: "Products Builder – Full-Stack (MERN)",
-      description: "A solo full-stack product management system with 100% CRUD coverage, responsive UI tested on 10+ screen sizes, and a Node.js/Express back end integrated with MongoDB supporting 1000+ product records via 5+ RESTful APIs.",
-      tags: ["React", "TypeScript", "Node.js", "Express", "MongoDB", "Tailwind CSS"],
-      image: productBuilder
-    },
-    {
       title: "Mohamed Wael Clinic – Medical Website",
       description: "A front-end medical clinic website enabling patients to access services and book appointments. Features 6 main pages with 18+ content sections, including a homepage with 9+ key sections. Integrated a custom WordPress + PHP form system generating 100+ monthly requests. Fully responsive across mobile, tablet, and laptop.",
       tags: ["WordPress", "PHP", "CSS", "Responsive Design"],
       image: mohamedWael
+    },
+    {
+      title: "Products Builder – Full-Stack (MERN)",
+      description: "A solo full-stack product management system with 100% CRUD coverage, responsive UI tested on 10+ screen sizes, and a Node.js/Express back end integrated with MongoDB supporting 1000+ product records via 5+ RESTful APIs.",
+      tags: ["React", "TypeScript", "Node.js", "Express", "MongoDB", "Tailwind CSS"],
+      image: productBuilder
     }
   ]
 
